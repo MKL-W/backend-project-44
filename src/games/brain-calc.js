bb54c80ff668356c3d970733a14f9f6f9ee65fbd
mkl-w <mkl-white@mail.ru>
2022-12-21 19:40:35 +0100

    modified

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,5 +1,4 @@
-import readlineSync from 'readline-sync';
-import { hello, areUserAnswerTry } from '../index.js';
+import engaine from '../index.js';
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -7,50 +6,24 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
 }
 
-const brainCalc = () => {
-  //hello();
+const randomNum1 = Math.round(Math.random() * 100);
+const randomNum2 = Math.round(Math.random() * 100);
+const operators = ['+', '-', '*'];
+const randomOp = operators[getRandomIntInclusive(0, 2)];
 
-  for (let counter = 0; counter < 3; counter += 1) {
-    const randomNum1 = Math.round(Math.random() * 100);
-    const randomNum2 = Math.round(Math.random() * 100);
-    const operators = ['+', '-', '*'];
-    const randomOp = operators[getRandomIntInclusive(0, 2)];
+const rules = 'What is the result of the expression?';
+const taskForUser = `${randomNum1} ${randomOp} ${randomNum2}`;
 
-    const taskForUser = () => `${randomNum1} ${randomOp} ${randomNum2}`;
+const trueAnswer = () => {
+  switch (randomOp) {
+    case '+':
+      return randomNum1 + randomNum2;
 
-    const trueAnswer = () => {
-      switch (randomOp) {
-        case '+':
-          return randomNum1 + randomNum2;
-        case '-':
-          return randomNum1 - randomNum2;
-        default:
-          return randomNum1 * randomNum2;
-      }
-    };
-
-    const userAnswer = readlineSync.question(`Question: ${taskForUser()}\nYour answer: `);
-    if (Number(userAnswer) === trueAnswer()) {
-      console.log('Correct!');
-    } else {
-      return console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${trueAnswer()}.\nLet's try again`);
-    }
+    case '-':
+      return randomNum1 - randomNum2;
+    default:
+      return randomNum1 * randomNum2;
   }
-  console.log('Congratulations, MKL!');
 };
 
-brainCalc();
-
-/* const areUserAnswerTry = (taskForUser) => {
-  const userAnswer = readlineSync.question(`Question: ${taskForUser}\nYour answer: `);
-  if (Number(userAnswer) === trueAnswer()) {
-    return console.log('Correct!');
-  }
-  console.log('unCorrect!');
-}; */
-
-/* hello();
-
-console.log('What is the result of the expression?');
-
-userAnswer(taskForUser()); */
+engaine(rules, taskForUser, trueAnswer());
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import readlineSync from 'readline-sync';
 
-export const hello = () => {
-  console.log('Welcome to Brain Games!'); // приветствие
+export default (rules, taskForUser, trueAnswer) => {
+  console.log('Welcome to Brain Games!');
   const name = readlineSync.question('May I have you name? ');
-  return console.log(`Hello, ${name}!`);
-};
-
-export const areUserAnswerTry = (taskForUser, trueAnswer) => {
+  console.log(`Hello, ${name}!`);
+  console.log(rules);
   for (let counter = 0; counter < 3; counter += 1) {
-    taskForUser();
     const userAnswer = readlineSync.question(`Question: ${taskForUser}\nYour answer: `);
     if (Number(userAnswer) === trueAnswer) {
       console.log('Correct!');
@@ -16,14 +13,5 @@ export const areUserAnswerTry = (taskForUser, trueAnswer) => {
       return console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${trueAnswer}.\nLet's try again`);
     }
   }
+  console.log(`Congradulations, ${name}! `);
 };
-
-/* export const getUserAnswer = (taskForUser) => {
-  const userAnswer = readlineSync.question(`Question: ${taskForUser}\nYour answer: `);
-  return userAnswer;
-};
-
-const areUserAnswerTry = (getUserAnswer(), trueAnswer) => {
-    const result = (Number(getUserAnswer()) === trueAnswer) ? 'correct' : 'unCorrect';
-    return result;
-}; */
