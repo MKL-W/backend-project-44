9a4157753abbac6c1fceb38201ab32a5b26989e3
mkl-w <mkl-white@mail.ru>
2022-12-22 14:26:24 +0100

    modifed brain-calc&index

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,29 +1,36 @@
 import engaine from '../index.js';
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
-}
-
-const randomNum1 = Math.round(Math.random() * 100);
-const randomNum2 = Math.round(Math.random() * 100);
-const operators = ['+', '-', '*'];
-const randomOp = operators[getRandomIntInclusive(0, 2)];
-
 const rules = 'What is the result of the expression?';
-const taskForUser = `${randomNum1} ${randomOp} ${randomNum2}`;
 
-const trueAnswer = () => {
-  switch (randomOp) {
-    case '+':
-      return randomNum1 + randomNum2;
-
-    case '-':
-      return randomNum1 - randomNum2;
-    default:
-      return randomNum1 * randomNum2;
+const giveTaskAnswer = () => {
+  function getRandomIntInclusive(min, max) {
+    // eslint-disable-next-line no-param-reassign
+    min = Math.ceil(min);
+    // eslint-disable-next-line no-param-reassign
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
   }
+
+  const randomNum1 = Math.round(Math.random() * 100);
+  const randomNum2 = Math.round(Math.random() * 100);
+  const operators = ['+', '-', '*'];
+  const randomOp = operators[getRandomIntInclusive(0, 2)];
+  const result = [];
+  const taskForUser = () => (`${randomNum1} ${randomOp} ${randomNum2}`);
+
+  const trueAnswer = () => {
+    switch (randomOp) {
+      case '+':
+        return randomNum1 + randomNum2;
+      case '-':
+        return randomNum1 - randomNum2;
+      default:
+        return randomNum1 * randomNum2;
+    }
+  };
+
+  result.push(taskForUser(), trueAnswer());
+  return result;
 };
 
-engaine(rules, taskForUser, trueAnswer());
+engaine(rules, giveTaskAnswer);
