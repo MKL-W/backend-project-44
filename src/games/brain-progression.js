82e0945952fa32c93f79ff52d5712437ffe9e9a1
mkl-w <dasmullix@mail.ru>
2023-04-17 19:55:22 +0200

    fix code rev.4

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -5,7 +5,7 @@ const rules = 'What number is missing in the progression?';
 
 const createProgression = (firstElment, step, indexOfHiddenElement) => {
   const progression = [];
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < 6; i += 1) {
     progression.push(firstElment + (i * step));
   }
   progression[indexOfHiddenElement] = '..';
@@ -15,7 +15,7 @@ const createProgression = (firstElment, step, indexOfHiddenElement) => {
 const giveTaskAndTrueAnswer = () => {
   const firstElment = getRandomIntInclusive();
   const step = getRandomIntInclusive();
-  const indexOfHiddenElement = getRandomIntInclusive(0, 9);
+  const indexOfHiddenElement = getRandomIntInclusive(0, 5);
 
   const trueAnswer = firstElment + (step * indexOfHiddenElement);
 
